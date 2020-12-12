40948efb54391c5d56aecb06f32f63cd53cf8a52
Oscar <voscarmv@gmail.com>
2020-12-12 14:07:47 -0600

    Growndwork for script.

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Project setup
+
+# Clone the student's project/repository you want to test into your machine
+# Change your current directory to the repository you just cloned
+# Verify you are on the branch with the latest changes. (Usually, the one from which the pull request was made).
+
+# After completing the Project Setup:
+
+# Branch out from the branch with the latest changes ( git checkout -b test )
+# Add a new remote source. For this, run the command: git remote add new_origin THIS_REPO_URL
+
+# Merge the contents of your new remote source into the branch you created. Do it with the following command: git pull new_origin master --allow-unrelated-histories
+
+# run bundle install
+
